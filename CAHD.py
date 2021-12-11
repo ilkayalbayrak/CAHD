56a45566706bf50ddffe1007a2a5c6f4da9fd558
ilkayalbayrak <ilkayyalbayrak@gmail.com>
2021-12-11 21:24:42 +0100

    added candidate list computing

--- CAHD.py
+++ CAHD.py
@@ -31,13 +31,60 @@ class CAHD:
     def check_conflict(self, row_i, position_j):
         # function for checking any sensitive items in conflict in the same group
         # if there are sensitive items in common, there is conflict
+        row_j = list(self.band_matrix.iloc[position_j][self.sensitive_items])
+        for position in range(len(self.sensitive_items)):
+            if row_i[position] + row_j[position] > 1:
+                return True
+        return False
 
+    # def check_conflict(self, row_i, row_j):
+        # function for checking any sensitive items in conflict in the same group
+        # if there are sensitive items in common, there is conflict
+    #     sensitive_data_row_i = self.sensitive_items[np.where(self.band_matrix.iloc[row_i][self.sensitive_items] == 1)]
+    #     sensitive_data_row_i = self.sensitive_items[np.where(self.band_matrix.iloc[row_j][self.sensitive_items] == 1)]
+    #     # create set
+    #     set_j = set(sensitive_data_row_i)
+    #     set_i = set(sensitive_data_row_i)
+    #     # check intersection
+    #     return len(set_i.intersection(set_j)) > 0
 
+    # function for checking the final validity state of the group
+    # mainly for reducing the code redundancy in compute_candidate_list function
+    def check_list(self, i, idx_sensitive_transaction, k, candidate_list):
+        row_i = list(self.band_matrix.iloc[i][self.sensitive_items])
+        if self.check_conflict(row_i, idx_sensitive_transaction):
+            k = k + 1
+        else:
+            conflict_list = False
+            for index in candidate_list:
+                if self.check_conflict(row_i, index):
+                    conflict_list = True
+                    break
+            if not conflict_list:
+                candidate_list.append(i)
+            else:
+                k = k + 1
+        return k
 
-        pass
+    # calculate the list of candidate groups
+    def compute_candidate_list(self, idx_sensitive_transaction):
+        alpha_p = self.alpha_ * self.p_degree
+        candidate_list = list()
+        k = 1
+        i = idx_sensitive_transaction - 1
+        while i > max(idx_sensitive_transaction - alpha_p - k, -1):
+            k = self.check_list(i, idx_sensitive_transaction, k, candidate_list)
+            i -= 1
 
-    def candidate_list(self):
-        pass
+        k = 1
+        i = idx_sensitive_transaction + 1
+        while i < min(idx_sensitive_transaction + alpha_p + k, len(self.band_matrix)):
+            k = self.check_list(i, idx_sensitive_transaction, k, candidate_list)
+            i += 1
+        error = False
+        if len(candidate_list) < self.p_degree:
+            error = True
+        return candidate_list, error
 
     def anonymize(self):
         pass
@@ -45,5 +92,3 @@ class CAHD:
 
 if __name__ == "__main__":
     pass
-
-
