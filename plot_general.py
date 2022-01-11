1d7fbbf4eab3926613650697ff0177c1654e9e96
ilkayalbayrak <ilkayyalbayrak@gmail.com>
2022-01-11 21:40:09 +0100

    added plots for presentation

---
+++ plot_general.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from band_matrix import compute_band_matrix, logger
+
+if __name__ == "__main__":
+    BMS1_m10_privacy_ranged_df = pd.read_csv("./Data_to_plot/BMS1_1000_10.csv")
+    BMS1_m20_privacy_ranged_df = pd.read_csv("./Data_to_plot/BMS1_1000_20.csv")
+
+    BMS1_p10_sensitive_ranged = pd.read_csv("./Data_to_plot/BMS1_1000_10_num_sensitive_change.csv")
+    BMS1_p20_sensitive_ranged = pd.read_csv("./Data_to_plot/BMS1_1000_20_num_sensitive_change.csv")
+
+    logger("BMS1_m10_privacy_ranged_df", BMS1_m10_privacy_ranged_df)
+    logger("BMS1_m20_privacy_ranged_df", BMS1_m20_privacy_ranged_df)
+
+    logger("BMS1_p10_sensitive_ranged", BMS1_p10_sensitive_ranged)
+    logger("BMS1_p20_sensitive_ranged", BMS1_p20_sensitive_ranged)
+
+    # plot KLD vs p degree
+    plt.figure(figsize=(12, 8))
+
+    # BMS1  m 10
+    x_range = [6, 8, 10, 12, 14, 16, 18, 20]
+    BMS1_m10 = BMS1_m10_privacy_ranged_df[BMS1_m10_privacy_ranged_df["KLD_value"] > 0]
+    BMS1_m20 = BMS1_m20_privacy_ranged_df[BMS1_m20_privacy_ranged_df["KLD_value"] > 0]
+
+    BMS1_p10 = BMS1_p10_sensitive_ranged[BMS1_p10_sensitive_ranged["KLD_value"]>0]
+    BMS1_p20 = BMS1_p20_sensitive_ranged[BMS1_p20_sensitive_ranged["KLD_value"]>0]
+    logger("XXXXXXXX", BMS1_m10)
+
+    plt.plot(BMS1_m10["p_degree"], BMS1_m10["KLD_value"], marker='o', linestyle='-', color='b', label="m=10")
+    plt.plot(BMS1_m20["p_degree"], BMS1_m20["KLD_value"], marker='^', linestyle='-', color='r', label="m=20")
+    plt.ylim(ymax=0.1)
+    plt.ylim(ymin=0)
+    # plt.xlim(xmin=0)
+    plt.xlabel("Privacy degree")
+    plt.ylabel("KL_Divergence")
+    plt.xticks(x_range, x_range)
+    plt.title("KLD values over a range of p degrees (r = 4) BMS1 m = 10 & m = 20 ")
+    plt.legend()
+    plt.savefig('./Plots/BMS1_p_vs_KLD.png')
+    plt.show()
+
+    x_range = [4, 6, 8, 10, 12, 14, 16, 18, 20]
+    plt.figure(figsize=(12, 8))
+    plt.plot(BMS1_p10["num_sensitive"], BMS1_p10["KLD_value"], marker='o', linestyle='-', color='b', label="p=10")
+    plt.plot(BMS1_p20["num_sensitive"], BMS1_p20["KLD_value"], marker='^', linestyle='-', color='r', label="p=20")
+    plt.ylim(ymax=0.2)
+    plt.ylim(ymin=0)
+    # plt.xlim(xmin=0)
+    plt.xlabel("Number of Sensitive Items")
+    plt.ylabel("KL_Divergence")
+    plt.xticks(x_range, x_range)
+    plt.title("KLD values over a range of p degrees (r = 4) BMS1 m = 10 & m = 20 ")
+    plt.legend()
+    plt.savefig('./Plots/BMS1_m_vs_KLD.png')
+    plt.show()
+    # plt.title("Reconstruction Error vs p (r = 4) BMS1 m = 10 & m = 20 ")
+    # plot KLD vs num sensitive
+    # plot computation times
+    # plot band matrix
